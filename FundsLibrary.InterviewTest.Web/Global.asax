1d18ce54460651e37f9b161a920b2abc92684a3b
Robert Adams <Robert.Adams@fundslibrary.co.uk>
2015-02-19 19:35:18 +0000

    Added default VS MVC template project & tests.

---
+++ FundsLibrary.InterviewTest.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FundsLibrary.InterviewTest.Web.MvcApplication" Language="C#" %>
---
+++ FundsLibrary.InterviewTest.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FundsLibrary.InterviewTest.Web.Startup))]
+namespace FundsLibrary.InterviewTest.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
