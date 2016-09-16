b83549fc45781be5aad6c2b97eb105036f0141be
Marc Howard <marc.howard@coutts.com>
2016-09-16 11:49:19 +0100

    Add files via upload

---
+++ FundsLibrary.InterviewTest.Web/Models/CustomRole.cs
@@ -0,0 +1,36 @@
+﻿using FundsLibrary.InterviewTest.Web.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FundsLibrary.InterviewTest.Web.Models
+{
+    public class CustomRole
+    {
+        private readonly IUserModelRepository _repository;
+
+        public CustomRole()
+            : this(new UserModelRepository())
+        { }
+
+        public CustomRole(IUserModelRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> IsUserInRole(Guid id, string role)
+        {
+            var user = await _repository.Get(id);
+            if (id != null)
+            {
+                if (user.Role.ToString() == role)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ FundsLibrary.InterviewTest.Web/Repositories/UserModelRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FundsLibrary.InterviewTest.Common;
+using FundsLibrary.InterviewTest.Web.Models;
+using FundsLibrary.InterviewTest.Web.Models.Mappers;
+
+namespace FundsLibrary.InterviewTest.Web.Repositories
+{
+    public interface IUserModelRepository
+    {
+        Task<Guid> Post(UserModel User);
+        Task<IEnumerable<UserModel>> GetAll();
+        Task<UserModel> Get(Guid id);
+    }
+
+    public class UserModelRepository : IUserModelRepository
+    {
+        private const string _serviceAppUrl = "http://localhost:50135/Service/";
+
+        private readonly IHttpClientWrapper _client;
+        private readonly IMapper<User, UserModel> _toModelMapper;
+
+        public UserModelRepository(
+            IHttpClientWrapper client = null,
+            IMapper<User, UserModel> toModelMapper = null)
+        {
+            _client = client ?? new HttpClientWrapper(_serviceAppUrl);
+            _toModelMapper = toModelMapper ?? new ToUserModelMapper();
+        }
+
+        public async Task<Guid> Post(UserModel content)
+        {
+            var user = await _client.PostContentAndGetAsync<Guid>("api/User/", content);
+            return user;
+        }
+
+        public async Task<IEnumerable<UserModel>> GetAll()
+        {
+            var users = await _client.GetAndReadFromContentGetAsync<IEnumerable<User>>("api/User/");
+            return users.Select(s => _toModelMapper.Map(s)).OrderBy(s => s.RegisteredSince);
+        }
+
+        public async Task<UserModel> Get(Guid id)
+        {
+            var user = await _client.GetAndReadFromContentGetAsync<User>("api/User/"+id);
+            return _toModelMapper.Map(user);
+        }
+    }
+}
